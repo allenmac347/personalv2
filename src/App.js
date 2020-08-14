d0bc0ec053247daf24ffa6554dc80fabf14d0d0e
amaca <amaca@umich.edu>
2020-08-14 05:42:25 -0400

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import {
+  Menu
+} from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render(){
+    return (
+      <div class='content'>
+        <NavigationBar/>
+        <Welcome/>
+      </div>
+    );
+  }
+}
+
+class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      active: 'none',
+    };
+  }
+
+  render() {
+    return(
+      <div id='navbar'>
+        <Menu inverted>
+          <Menu.Item 
+            header = {true}
+            active = {this.state.active == 'main'}
+            onClick={() => this.setState({active: 'main'})}
+            name = 'AM'
+            position = 'left'
+          />
+          <Menu.Item
+            position = 'right'
+            name='About Me'
+            active={this.state.active == 'about'}
+            onClick={() => this.setState({active: 'about'})}
+          />
+          <Menu.Item
+            name='Projects'
+            active={this.state.active == 'project'}
+            onClick={() => this.setState({active: 'project'})}
+          />
+          <Menu.Item
+            name='Contact'
+            active={this.state.active == 'contact'}
+            onClick={() => this.setState({active: 'contact'})}
+          />
+        </Menu>
+      </div>
+    );
+  }
+}
+
+function Welcome(){
+  return(
+    <div id='welcomePage'>
+      <h1>Welcome!</h1>
+      <h2>My name is Allen Macaspac</h2>
     </div>
   );
 }
